99df200c258db41976a50c03f34e6e9b2a1791a7
OlidaHirma <celestinendong8@gmail.com>
2026-03-06 10:03:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Severance Request / Response</title>
+</head>
+<body>
+
+<h1>Olida's Severance Request / Response</h1>
+<p>The SHA-256 hash of 'Olida' is:</p>
+<?php
+    print hash('sha256', 'Stephen');
+?>
+<p>ASCII ART BY Olida</p>
+
+<pre>
+    O O O O O O O O 
+   O               O
+   O               O
+   O               O
+   O               O
+   O               O
+    O O O O O O O O 
+</pre>
+
+<li><a href="./fail.php">click here check the error setting</a></li>
+<li><a href="./checkout.php">click here to cause a traceback</a></li>
+</body>
+</html>
